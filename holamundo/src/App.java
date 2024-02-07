f5e3709435fb17af6099711136eb1e279c565b5c
William Alviz <115000755+walviz@users.noreply.github.com>
2024-02-07 15:24:49 +0000

    dos

--- holamundo/src/App.java
+++ holamundo/src/App.java
@@ -1,12 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String cadena ="variable de texto";
-        int numero = 7;
-        double decimal = 0.5;
 
-        System.out.println("hola pesado de ingeniero");
-        System.out.println(cadena);
-        System.out.println(numero);
-        System.out.println(decimal);
+        Clase0 metodo= new Clase0();
+        metodo.metodo1();
     }
 }
---
+++ holamundo/src/Clase0.java
@@ -0,0 +1,8 @@
+
+public class Clase0 {
+
+    
+    public void metodo1(){
+        System.out.println("Desde Metodo");
+    }
+}
